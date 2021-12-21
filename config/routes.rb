072c4f5e47471b7db93242d8bebb476201bec2ef
dorji_gyeltshen <dorji.gyeltshen@selise.ch>
2021-12-21 23:09:11 +0600

    Graphiql

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+
   scope 'api/v1' do
     devise_for :users, controllers: {
       sessions: "api/v1/sessions"
@@ -6,8 +8,4 @@ Rails.application.routes.draw do
   end
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  end
 end
