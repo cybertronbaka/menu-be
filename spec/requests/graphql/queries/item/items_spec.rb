6c75656ee1781bc6ab46032e0fae5845685e7a14
dorji_gyeltshen <dorji.gyeltshen@selise.ch>
2021-12-22 23:14:26 +0600

    Specs for Queries

---
+++ spec/requests/graphql/queries/item/items_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+describe 'Queries: items' do
+  let!(:user) { create(:user) }
+  let!(:owner1) { create(:user, :restuarant_owner) }
+  let!(:menu) { create(:menu, user: owner1) }
+  let!(:section) { create(:section, menu: menu) }
+  let!(:item) { create(:item, section: section, rank: 2) }
+  let!(:item2) { create(:item, section: section, rank: 1) }
+  let!(:query) do
+    <<~GQL
+      query{
+        items(sectionId: #{section.id}){
+          id
+          name
+          description
+          rank
+          status
+          price
+          pictures
+          createdAt
+          updatedAt
+          sectionId
+        }
+      }
+    GQL
+  end
+
+  context 'Success' do
+    it 'Listing Items' do
+      sign_in(owner1)
+      post '/graphql', params: { query: query }
+      expect_no_gql_errors
+      expect(parsed[:data]).to_not be_nil
+      expect(parsed.dig(:data, :items).count).to eq(2)
+      expect(parsed.dig(:data, :items).pluck(:id)).to match_array(%w[2 1])
+    end
+  end
+end
