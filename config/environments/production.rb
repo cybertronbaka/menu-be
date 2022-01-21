08a84d642d5372bb5ac34612e68432c729005789
dorji_gyeltshen <dorji.gyeltshen@selise.ch>
2022-01-21 12:46:30 +0600

    Cookie Cross-site for testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,4 +112,12 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.middleware.use(
+    ActionDispatch::Session::CookieStore,
+    key: '_nye_menu_session',
+    expire_after: 1.day,
+    serializer: :json,
+    secure: true,
+    same_site: :none
+  )
 end
