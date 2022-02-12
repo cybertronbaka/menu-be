7325782eb62940aa4d2f295d3db01fa11059ef0d
dorji_gyeltshen <dorji.gyeltshen@selise.ch>
2022-02-12 21:47:38 +0600

    Delete Cookie Serializer and session store

--- config/initializers/session_store.rb
+++
@@ -1,5 +0,0 @@
-if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: '_nye_menu_session', domain: 'devapi.nyemenu.com'
-else
-  Rails.application.config.session_store :cookie_store, key: '_nye_menu_session'
-end
