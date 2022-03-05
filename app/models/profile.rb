cf05caaf4417e29458a9957b13df660c2368174e
dorji_gyeltshen <dorji.gyeltshen@selise.ch>
2022-03-05 22:20:51 +0600

    Profile Updation should update menu

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,16 @@ class Profile < ApplicationRecord
   belongs_to :user
 
   after_create :create_menu
+  before_update :update_name_in_menu, if: :name_changed?
+  before_update :update_description_in_menu, if: :description_changed?
+
+  def update_name_in_menu
+    user.menus.update_all(name: name)
+  end
+
+  def update_description_in_menu
+    user.menus.update_all(description: description)
+  end
 
   def create_menu
     return if user.role_id == 1
